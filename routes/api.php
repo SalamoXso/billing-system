e646f3d40c44d6a018ac8de3ebd853ae832e2aa1
Salamo <subfornothing1994@gamil.com>
2025-05-10 03:55:13 +0200

    123467

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/invoices/{invoice}/details', function (App\Models\Invoice $invoice) {
+    return response()->json([
+        'success' => true,
+        'client_id' => $invoice->client_id,
+        'client_name' => $invoice->client->name,
+        'balance' => $invoice->balance
+    ]);
+});
